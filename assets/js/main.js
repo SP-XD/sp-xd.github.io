6bd8ace595c1f6a407a3e34cea8c4016c1daf40f
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-08-16 19:02:07 +0530

    feat: added nav bar shadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,13 @@ function scrollActive(){
 
 window.addEventListener('scroll', scrollActive)
 /*---------------- CHANGE BACKGROUND HEADER ----------------*/ 
+function scrollHeader() {
+    const nav = document.getElementById('header')
 
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*---------------- SHOW SCROLL UP ----------------*/ 
 
