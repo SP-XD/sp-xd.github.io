90b2d71390ebedd3cc8ab60927da28ee4cf3c10e
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-09-04 20:59:10 +0530

    feat: implemented blog articles using api

--- main.js
+++ main.js
@@ -164,6 +164,63 @@ var swiper = new Swiper(".projects_content", {
     },
   },
 });
+/*-------------------------- BLOGS ----------------------------*/
+// Fetch function
+async function gql(query, variables={}) {
+    const data = await fetch('https://api.hashnode.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables
+        })
+    });
+
+    return data.json();
+}
+// Query 
+const GET_USER_ARTICLES = `
+    query GetUserArticles($page: Int!) {
+        user(username: "sp-xd") {
+            publication {
+                posts(page: $page) {
+                    title
+                    brief
+                    slug
+                }
+            }
+        }
+    }
+`;
+// making the call and injecting the articles
+gql(GET_USER_ARTICLES, { page: 0 })
+    .then(result => {
+        const articles = result.data.user.publication.posts;
+        let container = document.createElement('div');
+
+        articles.forEach(article => {
+            let title = document.createElement('h2');
+            title.innerText = article.title;
+
+            let brief = document.createElement('p');
+            brief.innerText = article.brief;
+
+            let link = document.createElement('a');
+            link.innerText = 'Read more...';
+            link.target = '_blank';
+            link.href = `https://sp-xd.hashnode.dev/${article.slug}`;
+
+            container.appendChild(title);
+            container.appendChild(brief);
+            container.appendChild(link);
+        })
+
+        document.querySelector('.blogs_container').appendChild(container);
+});
+
+
 /*---------------- SCROLL SECTIONS ACTIVE LINK ----------------*/
 const sections = document.querySelectorAll("section[id]");
 
